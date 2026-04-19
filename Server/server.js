e8a33204c320f23ab84172d025b9b32e8f2005b0
Himanshu Rawat <HIMANSHU.S1205@GMAIL.COM>
2026-04-19 20:44:23 +0530

    render

--- Server/server.js
+++ Server/server.js
@@ -18,9 +18,10 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
 app.use(express.json());
 app.use(morgan("dev"));
 
